0f8e141266dbc1a4f1c834ea66c75718b29ce6ad
alex-shmyga <shmyga.87@gmail.com>
2013-08-15 20:34:53 +0300

    four first steps added

---
+++ TddKata/TDDKata.StringCalculator.Tests/StringCalculatorTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace TDDKata.StringCalculator.Tests
+{
+    [TestFixture]
+    public class StringCalculatorTests
+    {
+        private StringCalculator calculator;
+
+        [SetUp]
+        public void Init() {
+            calculator = new StringCalculator();
+        }
+
+        [Test]
+        public void Sum_EmpthyNumbers_ReturnZero()
+        {
+            Assert.AreEqual(0, calculator.Add(string.Empty));
+        }
+
+        [Test]
+        public void Sum_OneNumber_ReturnSameNumber()
+        {
+            Assert.AreEqual(1, calculator.Add("1"));
+        }
+
+        [Test]
+        public void Sum_TwoNumbers_ReturnSumNumers()
+        {
+            Assert.AreEqual(3, calculator.Add("1,2"));
+        }
+
+        [Test]
+        public void Sum_UnknownCountNumbers_ReturnSumNumers()
+        {
+            Assert.AreEqual(15, calculator.Add("1,2,3,4,5"));
+        }
+
+        [Test]
+        public void Sum_NewLineBetweenNumbers_ReturnSumNumers()
+        {
+            Assert.AreEqual(6, calculator.Add("1\n2,3"));
+        }
+
+        [Test]
+        [ExpectedException( typeof( FormatException ) )]
+        public void Sum_WrongInputNumbers_FormatExceptionThrown()
+        {
+            Assert.AreEqual(6, calculator.Add(@"1, \ N" ));
+        }
+
+        [Test]
+        public void Sum_DifferentDelimiters_ReturnSumNumers()
+        {
+            Assert.AreEqual(3, calculator.Add("//;\n1;2"));
+        }
+
+        
+    }
+}
---
+++ TddKata/TDDKata.StringCalculator/StringCalculator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace TDDKata.StringCalculator
+{
+    public class StringCalculator
+    {
+        private int[] ConvertToIntArray(string[] stringsArray)
+        {
+            IList<int> result = new List<int>();
+            foreach (var number in stringsArray)
+            {
+                result.Add(int.Parse(number));
+            }
+            return result.ToArray();
+        }
+
+        public int Add(string numbers)
+        {         
+            if (numbers == string.Empty)
+            {
+                return 0;
+            }
+            var pattern = @"\/\/.\n";            
+            var delimiters = new Collection<char>{',','\n'};
+            MatchCollection matches = Regex.Matches(numbers, pattern, RegexOptions.Singleline);
+            if (matches.Count > 0) {
+                delimiters.Add(matches[0].ToString()[2]);
+                numbers = numbers.Remove(0, 4);
+            }
+            int[] numbersArray = ConvertToIntArray(numbers.Split(delimiters.ToArray()));
+            return numbersArray.Sum();
+        }
+    }
+}
